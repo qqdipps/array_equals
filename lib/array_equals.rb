8a78ecff4c24fa3e4fe82cc20daeda69d6b16472
qqdipps <sdipp007@gmail.com>
2019-02-22 21:22:17 -0800

    added missing edge unit tests and fixed algo

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -2,10 +2,10 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  array1_length = array1 ? array1.length : 0
-  if array1_length == (array2 ? array2.length : 0)
-    return true if array1_length == 0
-    (array1_length - 1).times do |i|
+  array1_length = array1 ? array1.length : -1
+  if array1_length == (array2 ? array2.length : -1)
+    return true if array1_length <= 0
+    array1_length.times do |i|
       return false if array1[i] != array2[i]
     end
   else
